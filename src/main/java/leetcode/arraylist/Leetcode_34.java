25463f088900a38c34ff795b70f1793ef4475d10
Liuhang Ji <1143528540@qq.com>
2025-06-01 21:25:02 +1000

    二分查找及其相关题目

--- src/main/java/leetcode/arraylist/Leetcode_34.java
+++ src/main/java/leetcode/arraylist/Leetcode_34.java
@@ -3,11 +3,10 @@ package leetcode.arraylist;
 public class Leetcode_34 {
     /**
      * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
-     * <p>
      * 如果数组中不存在目标值 target，返回 [-1, -1]。
-     * <p>
      * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
      *
+     * 方法一：先使用二分查找算法找到target的位置，再向左右分别移动指针
      * @param nums
      * @param target
      * @return
@@ -45,11 +44,11 @@ public class Leetcode_34 {
     }
 
     /**
-     * 使用双二分法分别查找左边界和右边界
+     * 方法二：使用两个二分查找分别查找左边界和右边界
      * 重点：
      *  1.要理解怎么找的左边界和右边界
      *  2.找到左右边界之后，要判断是否存在目标值：
-     *      情况一：leftborder=-2 以及rightborder=-2 说明数组中不存在目标值
+     *      情况一：leftborder = -2 以及 rightborder = -2 说明数组中不存在目标值
      *      情况二：必须满足 rightborder - leftBorder > 1 因为 [leftBorder,rightBorder] 区间内起码有一个值，且这个值为target
      *      情况三：如果rightborder - leftBorder <= 1，说明[leftBorder,rightBorder] 区间内没有值，也就是没有target
      * @param nums
